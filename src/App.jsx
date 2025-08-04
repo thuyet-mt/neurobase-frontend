62d4f45888458076569d41846d3c7aefe51cd212
thuyet-mt <maithuyetedu@gmail.com>
2025-08-04 23:25:13 +0700

    Remove error

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,14 @@ import { LanguageProvider } from "./contexts/LanguageContext";
 import ErrorBoundary from "./services/ErrorBoundary";
 import NotificationSystem from "./components/NotificationSystem";
 import Cursor3D from "./components/Cursor3D";
-import Cursor3DController from "./components/Cursor3DController";
 import { POSITION_CONFIG } from "./constants/buttons";
 
 function App() {
-  // Check if we're on test page
-  const isTestPage = window.location.pathname === '/test';
-  
-  if (isTestPage) {
-    return (
-      <ErrorBoundary>
-        <LanguageProvider>
-          <ThemeProvider>
-            <Cursor3D />
-            <Cursor3DController />
-            <LogoTestPage />
-          </ThemeProvider>
-        </LanguageProvider>
-      </ErrorBoundary>
-    );
-  }
-
   return (
     <ErrorBoundary>
       <LanguageProvider>
         <ThemeProvider>
           <Cursor3D />
-          <Cursor3DController />
           <NotificationSystem />
           <Neurobase 
             {...POSITION_CONFIG}
